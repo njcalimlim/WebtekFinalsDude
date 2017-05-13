9f092bcc542ca02c4a95e780ec2fd1001d254b8e
Richard Marbella <buboymarbella@gmail.com>
2017-05-13 15:22:57 +0800

    no message

--- Administrator/application/controllers/Register.php
+++ Administrator/application/controllers/Register.php
@@ -2,10 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller {
-        public function __construct(){
-            parent::__construct();
-        }
+    public function __construct()
+    {
+        parent::__construct();
+    }
     public function index (){
-        $this->load->view('register');
+        $this->load->view("register");
     }
 }
